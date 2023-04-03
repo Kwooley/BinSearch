24c450b46698958d6b6a062c2e779f8e5143ea6d
Kyu Lee <kwooler@gmail.com>
2023-04-03 00:56:17 -0700

    Binary Search

---
+++ bin_iter.py
@@ -0,0 +1,29 @@
+def binsearch(numbers, target):
+    left = 0
+    right = len(numbers)-1
+
+    while left <= right:
+        mid = (left+right) // 2
+        if numbers[mid] == target:
+            return mid
+        elif numbers[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    else:
+        return -1
+
+
+def main():
+    numbers = [10, 20, 25, 30, 50, 70, 80]
+    target = 70
+
+    ret = binsearch(numbers, target)
+    if ret != -1:
+        print(f'Found {target} at the index {ret}')
+    else:
+        print('Not Found')
+
+
+if __name__ == '__main__':
+    main()
